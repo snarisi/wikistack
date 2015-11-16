0645244a4609eb9b365d80286dccabfbc5b549a4
Sam Narisi <snarisi@gmail.com>
2015-11-16 17:45:56 -0500

    Add views and models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+// Notice the `mongodb` protocol; Mongo is basically a kind of server,
+// which handles database requests and sends responses. It's async!
+mongoose.connect('mongodb://localhost/wikistack'); // <= db name will be 'wikistack'
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
