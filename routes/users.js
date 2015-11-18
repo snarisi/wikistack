81a8f679dcce41774a3221887634c1e37e886f07
Sam Narisi <snarisi@gmail.com>
2015-11-17 19:22:28 -0500

    Use populate for single page view

--- routes/users.js
+++ routes/users.js
@@ -16,17 +16,29 @@ router.get('/:id', function(req, res) {
   var userId = req.params.id;
   var username;
   
-  User.findById(userId)
-    .exec()
-    .then(function(user) {
-      username = user.name;
-      return Page.find({ author: userId }).exec();
-    })
-    .then(function(pages) {
-      res.render(
-        'index', 
-        {title: username + "'s pages", pages: pages }
-      );
+//  User.findById(userId)
+//    .exec()
+//    .then(function(user) {
+//      username = user.name;
+//      return Page.find({ author: userId }).exec();
+//    })
+//    .then(function(pages) {
+//      res.render(
+//        'index', 
+//        {title: username + "'s pages", pages: pages }
+//      );
+//  });
+  
+  Page.find({ author: userId })
+  .populate('author')
+  .exec()
+  .then(function(pages) {
+    console.log(pages);
+    res.render(
+      'index', 
+      { title : pages[0].author.name + "'s pages", 
+       pages: pages 
+      });
   });
 });
 
